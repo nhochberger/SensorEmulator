712107b827badc65d5594c6592db42065f918267
nhochberger <nico@irrwisch.net>
2016-08-22 14:57:15 +0200

    #2

--- src/model/sensors/TrigonometryAwareSimpleRayTracingLidar.java
+++ src/model/sensors/TrigonometryAwareSimpleRayTracingLidar.java
@@ -16,6 +16,10 @@ public class TrigonometryAwareSimpleRayTracingLidar extends SimpleRayTracingLida
         return 50;
     }
 
+    private double openingAngle() {
+        return 15.0;
+    }
+
     @Override
     public HeightMap createTargetHeightMap(final Position position, final Vector3D direction) {
         final HeightMap map = new HeightMap(2 * beamRadius() + 1);
@@ -33,8 +37,8 @@ public class TrigonometryAwareSimpleRayTracingLidar extends SimpleRayTracingLida
     private double calculateStepSize(final Position position, final Vector3D vectorInDirection) {
         final Vector3D soundingLineVector = new Vector3D(0, -1, 0);
         final double angle = soundingLineVector.angleTo(vectorInDirection);
-        final double angle1 = angle + 15.0;
-        final double angle2 = angle - 15.0;
+        final double angle1 = angle + openingAngle();
+        final double angle2 = angle - openingAngle();
         logger().info("Angle between " + soundingLineVector + " and " + vectorInDirection + ": " + angle);
         final double deltaX1 = Math.tan(Math.toRadians(angle1)) * position.getY();
         final double deltaX2 = Math.tan(Math.toRadians(angle2)) * position.getY();
