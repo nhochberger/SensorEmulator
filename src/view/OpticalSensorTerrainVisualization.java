a3b53e21490ed9890394810ccbd771fd980fc191
nhochberger <nico@irrwisch.net>
2016-09-20 10:15:43 +0200

    workson #5
    - switched back to quads (looks nicer and more realistic)

--- src/view/OpticalSensorTerrainVisualization.java
+++ src/view/OpticalSensorTerrainVisualization.java
@@ -157,62 +157,23 @@ public class OpticalSensorTerrainVisualization implements GLEventListener {
     }
 
     protected void drawSurface(final GL2 gl) {
-        gl.glBegin(GL2.GL_TRIANGLES);
+        gl.glBegin(GL2.GL_QUADS);
         final TextureCoords coords = this.texture.getImageTexCoords();
         for (int z = 0; z < this.points.getZDimension() - 1; z++) {
             for (int x = 0; x < this.points.getXDimension() - 1; x++) {
-                final double distance1 = Math.abs(this.points.get(x, z) - this.points.get(x + 1, z + 1));
-                final double distance2 = Math.abs(this.points.get(x + 1, z) - this.points.get(x, z + 1));
-
-                if (distance1 < distance2) {
-                    gl.glTexCoord2d(coords.bottom(), coords.left());
-                    gl.glVertex3d(2 * x, this.points.get(x, z), 2 * z);
-                    gl.glTexCoord2d(coords.bottom(), coords.right());
-                    gl.glVertex3d(2 * (x + 1), this.points.get(x + 1, z + 1), 2 * (z + 1));
-                    gl.glTexCoord2d(coords.top(), coords.right());
-                    gl.glVertex3d(2 * (x + 1), this.points.get(x + 1, z), 2 * z);
-                    final float[] one1 = { 1, (float) (this.points.get(x, z + 1) - this.points.get(x, z)), 0 };
-                    final float[] two1 = { 1, (float) (this.points.get(x + 1, z + 1) - this.points.get(x, z)), 1 };
-                    float[] normal1 = new float[3];
-                    normal1 = VectorUtil.crossVec3(normal1, two1, one1);
-                    gl.glNormal3f(normal1[0], normal1[1], normal1[2]);
-
-                    gl.glTexCoord2d(coords.bottom(), coords.left());
-                    gl.glVertex3d(2 * x, this.points.get(x, z), 2 * z);
-                    gl.glTexCoord2d(coords.bottom(), coords.right());
-                    gl.glVertex3d(2 * x, this.points.get(x, z + 1), 2 * (z + 1));
-                    gl.glTexCoord2d(coords.top(), coords.right());
-                    gl.glVertex3d(2 * (x + 1), this.points.get(x + 1, z + 1), 2 * (z + 1));
-                    final float[] one2 = { 0, (float) (this.points.get(x, z + 1) - this.points.get(x, z)), 1 };
-                    final float[] two2 = { 1, (float) (this.points.get(x + 1, z + 1) - this.points.get(x, z)), 1 };
-                    float[] normal2 = new float[3];
-                    normal2 = VectorUtil.crossVec3(normal2, one2, two2);
-                    gl.glNormal3f(normal2[0], normal2[1], normal2[2]);
-                } else {
-                    gl.glTexCoord2d(coords.top(), coords.right());
-                    gl.glVertex3d(2 * (x + 1), this.points.get(x + 1, z + 1), 2 * (z + 1));
-                    gl.glTexCoord2d(coords.top(), coords.left());
-                    gl.glVertex3d(2 * (x + 1), this.points.get(x + 1, z), 2 * z);
-                    gl.glTexCoord2d(coords.bottom(), coords.right());
-                    gl.glVertex3d(2 * x, this.points.get(x, z + 1), 2 * (z + 1));
-                    final float[] one1 = { -1, (float) (this.points.get(x, z + 1) - this.points.get(x + 1, z + 1)), 0 };
-                    final float[] two1 = { 0, (float) (this.points.get(x + 1, z) - this.points.get(x + 1, z + 1)), -1 };
-                    float[] normal1 = new float[3];
-                    normal1 = VectorUtil.crossVec3(normal1, two1, one1);
-                    gl.glNormal3f(normal1[0], normal1[1], normal1[2]);
-
-                    gl.glTexCoord2d(coords.bottom(), coords.left());
-                    gl.glVertex3d(2 * x, this.points.get(x, z), 2 * z);
-                    gl.glTexCoord2d(coords.bottom(), coords.right());
-                    gl.glVertex3d(2 * x, this.points.get(x, z + 1), 2 * (z + 1));
-                    gl.glTexCoord2d(coords.top(), coords.left());
-                    gl.glVertex3d(2 * (x + 1), this.points.get(x + 1, z), 2 * z);
-                    final float[] one2 = { 0, (float) (this.points.get(x, z + 1) - this.points.get(x, z)), 1 };
-                    final float[] two2 = { 1, (float) (this.points.get(x + 1, z + 1) - this.points.get(x, z)), 0 };
-                    float[] normal2 = new float[3];
-                    normal2 = VectorUtil.crossVec3(normal2, one2, two2);
-                    gl.glNormal3f(normal2[0], normal2[1], normal2[2]);
-                }
+                gl.glVertex3d(2 * x, this.points.get(x, z), 2 * z);
+                gl.glTexCoord2d(coords.bottom(), coords.left());
+                gl.glVertex3d(2 * x, this.points.get(x, z + 1), 2 * (z + 1));
+                gl.glTexCoord2d(coords.top(), coords.left());
+                gl.glVertex3d(2 * (x + 1), this.points.get(x + 1, z + 1), 2 * (z + 1));
+                gl.glTexCoord2d(coords.top(), coords.right());
+                gl.glVertex3d(2 * (x + 1), this.points.get(x + 1, z), 2 * z);
+                gl.glTexCoord2d(coords.bottom(), coords.right());
+                final float[] one = { 0, (float) (this.points.get(x, z + 1) - this.points.get(x, z)), 1 };
+                final float[] two = { 1, (float) (this.points.get(x + 1, z) - this.points.get(x, z)), 0 };
+                float[] normal = new float[3];
+                normal = VectorUtil.crossVec3(normal, one, two);
+                gl.glNormal3f(normal[0], normal[1], normal[2]);
             }
         }
         gl.glEnd();
